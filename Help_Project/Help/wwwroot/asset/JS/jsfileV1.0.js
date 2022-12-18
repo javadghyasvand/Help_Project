a7e9091bc1f5930deac29f50e0f621af3e402348
javad ghyasvand <72897382+javadghyasvand@users.noreply.github.com>
2022-12-18 10:56:19 +0330

    commet

--- Help_Project/Help/wwwroot/asset/JS/jsfileV1.0.js
+++ Help_Project/Help/wwwroot/asset/JS/jsfileV1.0.js
@@ -19,3 +19,64 @@ $('#tabfigure .sub-menu > a').click(function () {
         sub.slideDown(200);
     }
 });
+$("#Btn-Col-Sidebar").click(function (e) {
+    Sidbar_Steate();
+});
+// sidbar show/hide
+function Sidbar_Steate() {
+    var width_screen = window.screen.width
+    if (!$(".panel-left").hasClass("none-display") == true) {
+        if (width_screen <= "750") {
+            $(".panel-left").css("width", "100%");
+        }
+        $(".panel-left").addClass("none-display");
+        $(".splitter").addClass("none-display");
+        switch ($("#lang_Help option:selected").val()) {
+            case "English":
+                $("#txt_sh-hi").text("Show");
+                break;
+            case "German":
+                $("#txt_sh-hi").text("zeigen");
+                break;
+            case "Spanish":
+                $("#txt_sh-hi").text("MOSTRAR");
+                break;
+            case "French":
+                $("#txt_sh-hi").text("montrer");
+                break;
+            case "Arabic":
+                $("#txt_sh-hi").text("عرض");
+                break;
+            case "Persian":
+                $("#txt_sh-hi").text("نمایش");
+                break;
+        }
+    } else {
+        if (width_screen <= "750") {
+
+            $(".panel-left").css("width", "100%");
+        }
+        $(".panel-left").removeClass("none-display");
+        $(".splitter").removeClass("none-display");
+        switch ($("#lang_Help option:selected").val()) {
+            case "English":
+                $("#txt_sh-hi").text("Hide");
+                break;
+            case "German":
+                $("#txt_sh-hi").text("Verstecken");
+                break;
+            case "Spanish":
+                $("#txt_sh-hi").text("Esconder");
+                break;
+            case "French":
+                $("#txt_sh-hi").text("cacher");
+                break;
+            case "Arabic":
+                $("#txt_sh-hi").text("أخفى");
+                break;
+            case "Persian":
+                $("#txt_sh-hi").text("مخفی");
+                break;
+        }
+    }
+}
